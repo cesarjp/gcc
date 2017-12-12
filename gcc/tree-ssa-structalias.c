f8ded7aef4fd65d6078323614da2775ba8acff73
Cesar Philippidis <cesar@codesourcery.com>
2017-12-12 14:53:16 -0800

    re-enable structalias analysis

--- gcc/tree-ssa-structalias.c
+++ gcc/tree-ssa-structalias.c
@@ -4616,9 +4616,9 @@ find_func_aliases_for_builtin_call (struct function *fn, gcall *t)
 	  return true;
 	}
       case BUILT_IN_GOMP_PARALLEL:
-	/* FIXME: enable alias analysis for GOACC_parallel_keyed.  */
-	//case BUILT_IN_GOACC_PARALLEL:
+      case BUILT_IN_GOACC_PARALLEL:
 	{
+	  bool oacc_parallel = false;
 	  if (in_ipa_mode)
 	    {
 	      unsigned int fnpos, argpos;
@@ -4632,12 +4632,16 @@ find_func_aliases_for_builtin_call (struct function *fn, gcall *t)
 		case BUILT_IN_GOACC_PARALLEL:
 		  /* __builtin_GOACC_parallel (device, fn, mapnum, hostaddrs,
 					       sizes, kinds, ...).  */
-		  fnpos = 1;
+		  fnpos = 2;
 		  argpos = 3;
+		  oacc_parallel = gimple_call_arg (t, 1) == integer_one_node;
 		  break;
 		default:
 		  gcc_unreachable ();
 		}
+
+	      if (oacc_parallel)
+		break;
 
 	      tree fnarg = gimple_call_arg (t, fnpos);
 	      gcc_assert (TREE_CODE (fnarg) == ADDR_EXPR);
@@ -5191,12 +5195,12 @@ find_func_clobbers (struct function *fn, gimple *origt)
 	  case BUILT_IN_VA_END:
 	    return;
 	  case BUILT_IN_GOMP_PARALLEL:
-	    /* FIXME: enable alias analysis for GOACC_parallel_keyed.  */
-	    //case BUILT_IN_GOACC_PARALLEL:
+	  case BUILT_IN_GOACC_PARALLEL:
 	    {
 	      unsigned int fnpos, argpos;
 	      unsigned int implicit_use_args[2];
 	      unsigned int num_implicit_use_args = 0;
+	      bool oacc_parallel = false;
 	      switch (DECL_FUNCTION_CODE (decl))
 		{
 		case BUILT_IN_GOMP_PARALLEL:
@@ -5207,14 +5211,18 @@ find_func_clobbers (struct function *fn, gimple *origt)
 		case BUILT_IN_GOACC_PARALLEL:
 		  /* __builtin_GOACC_parallel (device, fn, mapnum, hostaddrs,
 					       sizes, kinds, ...).  */
-		  fnpos = 1;
+		  fnpos = 2;
 		  argpos = 3;
-		  implicit_use_args[num_implicit_use_args++] = 4;
 		  implicit_use_args[num_implicit_use_args++] = 5;
+		  implicit_use_args[num_implicit_use_args++] = 6;
+		  oacc_parallel = gimple_call_arg (t, 1) == integer_one_node;
 		  break;
 		default:
 		  gcc_unreachable ();
 		}
+
+	      if (oacc_parallel)
+		break;
 
 	      tree fnarg = gimple_call_arg (t, fnpos);
 	      gcc_assert (TREE_CODE (fnarg) == ADDR_EXPR);
@@ -7969,9 +7977,8 @@ ipa_pta_execute (void)
 		tree called_decl = NULL_TREE;
 		if (gimple_call_builtin_p (stmt, BUILT_IN_GOMP_PARALLEL))
 		  called_decl = TREE_OPERAND (gimple_call_arg (stmt, 0), 0);
-		/* FIXME: enable alias analysis for GOACC_parallel_keyed.  */
-//		else if (gimple_call_builtin_p (stmt, BUILT_IN_GOACC_PARALLEL))
-//		  called_decl = TREE_OPERAND (gimple_call_arg (stmt, 1), 0);
+		else if (gimple_call_builtin_p (stmt, BUILT_IN_GOACC_PARALLEL))
+		  called_decl = TREE_OPERAND (gimple_call_arg (stmt, 2), 0);
 
 		if (called_decl != NULL_TREE
 		    && !fndecl_maybe_in_other_partition (called_decl))
