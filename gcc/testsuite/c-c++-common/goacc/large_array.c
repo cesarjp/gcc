8ffdf2bdc28c709631333bacf612ad68821fccf9
Cesar Philippidis <cesar@codesourcery.com>
2018-01-31 12:29:47 -0800

    fix an ICE involving alloca'ed arrays

---
+++ gcc/testsuite/c-c++-common/goacc/large_array.c
@@ -0,0 +1,16 @@
+/* Ensure that alloca'ed arrays can be transferred to the
+   accelerator. */
+
+int
+main ()
+{
+  int n = 100, m = 10, i, j;
+  float a[n][m];
+
+  #pragma acc parallel loop
+  for (i = 0; i < n; i++)
+    for (j = 0; j < m; j++)
+      a[i][j] = 0;
+
+  return 0;
+}
