d2d16808aac66ec03e10fff445cfe094685590cc
Cesar Philippidis <cesar@codesourcery.com>
2018-02-14 11:21:27 -0800

    update parallel-dims.c

--- libgomp/testsuite/libgomp.oacc-c-c++-common/parallel-dims.c
+++ libgomp/testsuite/libgomp.oacc-c-c++-common/parallel-dims.c
@@ -1,8 +1,7 @@
-/* { dg-xfail-if "hard-coded vector_length" { openacc_nvidia_accel_selected } } */
 /* OpenACC parallelism dimensions clauses: num_gangs, num_workers,
    vector_length.  */
 
-/* { dg-additional-options "-foffload-force" } */
+/* { dg-additional-options "-foffload-force -fopenacc-dim=-:-:32" } */
 
 #include <limits.h>
 #include <openacc.h>
